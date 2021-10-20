614a1098e36c3439dda622071e99f40ebec239fb
debutler266 <32465329+debutler266@users.noreply.github.com>
2021-10-20 19:12:43 -0400

    added the input for search bar, stored in state so search input can be called every time the API is searched/changed... also stored searchvalue in state to make the search bar actually work/search for values

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,88 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+
 
 const App = () => {
-  //**add state to obj to hold movie results when we search */}
-  const [movies, setMovies] = useState([
-    {
-            "Title": "Star Wars",
-            "Year": "1977",
-            "imdbID": "tt0076759",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode V - The Empire Strikes Back",
-            "Year": "1980",
-            "imdbID": "tt0080684",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VI - Return of the Jedi",
-            "Year": "1983",
-            "imdbID": "tt0086190",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VII - The Force Awakens",
-            "Year": "2015",
-            "imdbID": "tt2488496",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode I - The Phantom Menace",
-            "Year": "1999",
-            "imdbID": "tt0120915",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode III - Revenge of the Sith",
-            "Year": "2005",
-            "imdbID": "tt0121766",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode II - Attack of the Clones",
-            "Year": "2002",
-            "imdbID": "tt0121765",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VIII - The Last Jedi",
-            "Year": "2017",
-            "imdbID": "tt2527336",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode IX - The Rise of Skywalker",
-            "Year": "2019",
-            "imdbID": "tt2527338",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Solo: A Star Wars Story",
-            "Year": "2018",
-            "imdbID": "tt3778644",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOTM2NTI3NTc3Nl5BMl5BanBnXkFtZTgwNzM1OTQyNTM@._V1_SX300.jpg"
-        },
-      ]);
+  {/* add state to obj to hold movie results when we search */}
+  const [movies, setMovies] = useState([]);
+  {/* add input, store in in state so it can be called every time the search input changes for the API */}
+  const [SearchValue, setSearch] = useState('');
+
+      {/* Ad call to API to display more movies */}
+
+      const getMovieRequest = async () => {
+        const url = 'http://www.omdbapi.com/?s=spider&apikey=85ae2750';
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        console.log(responseJson);
+        setMovies(responseJson.Search);
+        {/* display movies from API search */}
+      };
+
+{/* getMovieRequest will fire off only when page is loading */}
+      useEffect(() => {
+        getMovieRequest();
+      }, []);
+
+
   return (
   <div className='container-fluid movie-app'>
-  <div className='row'>
+  <div className='row d-flex align-items-center mt-4 mb-4'>
+    <MovieListHeading heading='Movies'/>
+    {/* store SearchValuevalue in SearhBox state in order to make searchbar actually work */}
+    <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+    </div>
+    <div className='row'>
     <MovieList movies={movies} />
-  //** Call MovieList component & display it *//
-
+  {/* Call MovieList component & display it */}
     </div>
   </div>
   );
---
+++ movie-app/src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearhBox = (props) => {
+  return (
+    <div className='col col-sm-4'>
+    <input
+    className='form-control'
+    value={props.value}
+    onChange={(event) => props.setSearchValue(event.target.value)} 
+    placeholder='Start typing to search...'
+    ></input>
+    </div>
+  )
+}
+
+export default SearhBox;
